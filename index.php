b91931c55514b2d64171668527248042a8d7d184
Damian Gostomski <damian@mypocketmedia.co.uk>
2015-11-01 22:16:11 +0000

    Register the plugin

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Knife;
+
+/**
+ * Plugin Name: Pocket Knife
+ * Plugin URI:  https://github.com/pocket-media/pocket-knife
+ * Author:      Pocket Media
+ * Author URI:  http://mypocketmedia.co.uk
+ * Description: Development toolkit to speed up development of themes and sites.
+ * Version:     0.0.1
+ */
